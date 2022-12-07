bd3444221b7b63fc624ae93881bf1598e620d104
Kristián Kunc <kristian.kunc@gmail.com>
2022-12-07 17:20:09 +0100

    a

--- src/events/message-create.ts
+++ src/events/message-create.ts
@@ -44,6 +44,7 @@ async function handleXp(config: Config, message: Message) {
         
         LevelsDB.addXp(message.author.id, messageXp);
 
+        
         if (oldXp && await checkLevelUp(oldXp, oldXp + messageXp)) {
             let lvlupChannel = await message.guild?.channels.fetch(config.levels.levelupChannelId)
         
